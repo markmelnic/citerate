3972159692771d93f09f5148f3207c39fd33dbce
Mark Melnic <markhappy200@gmail.com>
2021-07-14 15:22:15 +0300

    Manual test added

---
+++ tests/manual_test.py
@@ -0,0 +1,27 @@
+import os, sys, inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+import citerate
+
+CLOCKWISE_SET = [
+    [10, 11, 12, 13, 14],
+    [25, 2,  3,  4,  15],
+    [24, 9,  1,  5,  16],
+    [23, 8,  7,  6,  17],
+    [22, 21, 20, 19, 18],
+]
+
+ANTICLOCKWISE_SET = [
+    [10, 25, 24, 23, 22],
+    [11, 2,  9,  8,  21],
+    [12, 3,  1,  7,  20],
+    [13, 4,  5,  6,  19],
+    [14, 15, 16, 17, 18],
+]
+
+if __name__ == "__main__":
+    for layer in citerate.citerator(CLOCKWISE_SET, i = 2, j = 2, clockwise=False):
+        print(layer)
